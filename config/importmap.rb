fc003ba5c97cc482ddbbbced9634a1fac57926f7
tatsumax <tatsumax.development@gmail.com>
2024-04-05 01:33:18 +0900

    ナビメニュー動作だけできるようにした

---
+++ config/importmap.rb
@@ -0,0 +1,10 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery" # @3.7.1
+pin "bootstrap" # @5.3.3
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/popper.min.js"
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  root 'task#index'
+end
