86e3df71c3e238e1ac29701073d4af51e72394c6
tatsumax <tatsumax.development@gmail.com>
2024-04-08 11:11:41 +0900

    userモデルのテストコード完成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,8 +11,32 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
-  test "名前が空の場合に無効であることあることを検証する" do
+  # name
+  test "名前が空のなら無効" do
     @user.name = ""
     assert_not @user.valid?
   end
+
+  # email
+  test "メールアドレスが空なら無効" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "メールアドレスの形式が不正だったら無効" do
+    @user.email = "hogehoge@hogehoge"
+    assert_not @user.valid?
+  end
+
+  # password
+  test "パスワードが空なら無効" do
+    @user.password = ""
+    assert_not @user.valid?
+  end
+
+  test "パスワードが8文字以下だったら無効" do
+    @user.password = 'a' * 5
+    assert_not @user.valid?
+  end
+
 end
