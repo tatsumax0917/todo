06090b24ed5ae223b9baa1e98cdf38ae86975153
tatsumax <tatsumax.development@gmail.com>
2024-05-06 16:09:18 +0900

    完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, # :confirmable(メール認証機能ON/OFF)
 
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, length: { maximum: 12 }
