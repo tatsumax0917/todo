c6f8dac023e1a5e55742a5d73f1fd902afd9d40e
tatsumax <tatsumax.development@gmail.com>
2024-05-16 03:45:02 +0900

    faild 3 migration file modified

--- db/migrate/20240410194302_change_column_default_to_tasks.rb
+++ db/migrate/20240410194302_change_column_default_to_tasks.rb
@@ -1,6 +1,5 @@
 class ChangeColumnDefaultToTasks < ActiveRecord::Migration[7.1]
   def change
-      change_column :tasks, :done, :integer, default: 0
-
+    change_column :tasks, :done, :boolean, default: false
   end
 end
