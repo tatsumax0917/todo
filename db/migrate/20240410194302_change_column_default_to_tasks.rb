04fe01b7d9fa1050240952fa3744d0c144f3c803
tatsumax <tatsumax.development@gmail.com>
2024-05-16 03:35:27 +0900

    faild 2 spell miss...

--- db/migrate/20240410194302_change_column_default_to_tasks.rb
+++ db/migrate/20240410194302_change_column_default_to_tasks.rb
@@ -1,6 +1,6 @@
 class ChangeColumnDefaultToTasks < ActiveRecord::Migration[7.1]
   def change
-      change_column :tasks, :done, :integerm, default: 0
+      change_column :tasks, :done, :integer, default: 0
 
   end
 end
