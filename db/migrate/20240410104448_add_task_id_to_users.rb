07801dd610370f0e8e5dcd9138edc958cf10bbbd
tatsumax <tatsumax.development@gmail.com>
2024-05-16 03:32:01 +0900

    faild->migration file delete 2 file

--- db/migrate/20240410104448_add_task_id_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddTaskIdToUsers < ActiveRecord::Migration[7.1]
-  def up
-    change_column :users, :task_id, :integer, null: false
-  end
-
-  def down
-    change_column :users, :task_id, :integer, null: true
-  end
-end
--- db/migrate/20240410110200_remove_users_task_id_column.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUsersTaskIdColumn < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :users, :task_id
-  end
-end
